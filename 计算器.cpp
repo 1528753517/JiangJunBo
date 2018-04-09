e218186af53e425d1755c19d74cbccecc45d6fd0
x1065607650 <1065607650@qq.com>
2018-04-09 16:16:10 +0800

    16点16分

--- 计算器.cpp
+++ 计算器.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
-#include<stack>
 using namespace std;
+template<class type>
+class stack
+{	
+	protected:
+		class node
+		{	
+			public:
+				type data;
+				node*next;
+		}*top;
+	public:
+		stack();
+		type get_top();
+		void push(type);
+		type pop();	
+		friend int evaluate();	
+};
 
-int check(char ch)
+template<class type>
+stack<type>::stack()
+{                   
+		top=new node;
+}
+
+template<class type>
+type stack<type>::get_top()
+{	
+	if(top)	
+		return top->data;
+}
+
+template<class type>
+void stack<type>::push(type data)
+{	
+	node*p;
+	p=new node;
+	p->data=data;
+    p->next=top;
+    top=p;
+}
+
+template<class type>
+type stack<type>::pop()
+{	
+	type data;
+	node*p;
+	p=new node;
+	p=top;
+	data=top->data;
+	top=top->next;
+	delete p;
+	return data;
+}
+
+int ln(char ch)
 {
     return(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='('||ch==')'||ch=='#');
 }
@@ -10,13 +62,13 @@ int check(char ch)
 char precede(char c1,char c2)
 {
 	int i=0,j=0;
-    static char array[49]={ '>', '>', '<', '<', '<', '>', '>',
-                   			'>', '>', '<', '<', '<', '>', '>',
-                    		'>', '>', '>', '>', '<', '>', '>',
-                     		'>', '>', '>', '>', '<', '>', '>',
-                     		'<', '<', '<', '<', '<', '=', '!',
-                     		'>', '>', '>', '>', '!', '>', '>',
-                     		'<', '<', '<', '<', '<', '!', '='};
+    static char a[49]={ '>', '>', '<', '<', '<', '>', '>',
+                        '>', '>', '<', '<', '<', '>', '>',
+                        '>', '>', '>', '>', '<', '>', '>',
+                        '>', '>', '>', '>', '<', '>', '>',
+                        '<', '<', '<', '<', '<', '=', '!',
+                        '>', '>', '>', '>', '!', '>', '>',
+                        '<', '<', '<', '<', '<', '!', '='};
     switch(c1)
     {
     case '+' :  i=0;break;
@@ -37,19 +89,18 @@ char precede(char c1,char c2)
     case ')' :  j=5;break;
     case '#' :  j=6;break;
     }
-    return (array[7*i+j]);
+    return (a[7*i+j]);
 }
 
- int run(int a,char c,int b) 
+int run(char c,int a,int b)
 {
-    switch(c)
-    {
-    case '+' :  return (a+b);
-    case '-' :  return (a-b);
-    case '*' :  return (a*b);
-    case '/' :  return (a/b);
-    }
-    return 0;
+	switch(c)
+	{
+		case'+':return a+b;break;
+		case'-':return a-b;break;
+		case'*':return a*b;break;
+		case'/':return a/b;break;
+	}
 }
 
 int evaluate()
@@ -58,34 +109,28 @@ int evaluate()
 	stack<int> stack_int;
 	stack_char.push('#');
 	int a,b;
-	char c,theta;
+	char c,theta,x;
 	cin>>c;
-	while(c!='#'||stack_char.top()!='#')	
-	{
-		if(!check(c))
+	while(c!='#'||(stack_char.get_top()!='#'))	
+		if(!ln(c))
 			{	
 				stack_int.push(c-'0');
 				cin>>c;
 			}
 		else
 		{
-			switch(precede(stack_char.top(),c))
+			switch(precede(stack_char.get_top(),c))
 			{
-				case'<':stack_char.push(c);cin>>c;;break;
-				case'=':stack_char.pop();cin>>c;;break;
-				case'>':theta=stack_char.top();stack_char.pop();
-						a=stack_int.top();stack_int.pop();
-						b=stack_int.top();stack_int.pop();
-						stack_int.push(run(b,theta,a));break;
+				case'<':stack_char.push(c);cin>>c;break;
+				case'=':x=stack_char.pop();cin>>c;break;
+				case'>':theta=stack_char.pop();a=stack_int.pop();b=stack_int.pop();stack_int.push(run(theta,b,a));break;
 			}
 		}
-	}
-	return stack_int.top();	
+	return stack_int.get_top();
 }
 
 int main()
 {	
-	cout<<"ÊäÈëÀý×Ó:3+2*1#"<<endl; 
 	cout<<evaluate();
 	return 0;
 }
